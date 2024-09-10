79926b40c8f04f1de8b63b3735729a3c2d105411
shohib <shohibshaikh369@gmail.com>
2024-09-10 16:07:36 +0530

    LinksUpdate

--- sample_Automation_Programs/src/test/java/testNG_Practice/TestNG2.java
+++ sample_Automation_Programs/src/test/java/testNG_Practice/TestNG2.java
@@ -1,6 +1,5 @@
 package testNG_Practice;
 
-import java.awt.Window;
 import java.time.Duration;
 import java.util.List;
 
@@ -9,8 +8,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
